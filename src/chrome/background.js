955d13dca47f376aeb7f486148c3f3ef4b97e4d5
Patrick Elam <swimpat16@gmail.com>
2024-01-31 10:05:49 -0600

    organizing files to allow for builder scripts

---
+++ src/chrome/background.js
@@ -0,0 +1,49 @@
+const PATTERN = "https://www.pivotaltracker.com/*";
+
+
+var handleSendEventResponse = (response) => {}
+
+var handleSendEventError = (e) => {
+    console.log("handleSendMessageError:");
+    console.log(e);
+}
+
+var sendEvent = (event, data, responseHandler, errorHandler) => {
+    chrome.tabs
+    .query({
+      currentWindow: true,
+      active: true,
+    })
+    .then((tabs) => {
+        for (const tab of tabs) {
+            chrome.tabs
+              .sendMessage(tab.id, { event : event, data : data })
+              .then(responseHandler, errorHandler);
+          }
+    });
+}
+
+var scrapeHeaders = (interceptedRequest) => {
+    interceptedRequest.requestHeaders.forEach((header) => {
+        if(header.name === "Cookie") {
+            sendEvent("set_token", {token : header.value}, handleSendEventResponse, handleSendEventError);
+        }
+    });
+}
+
+var handlePageFinishedLoading = (event) => {
+    sendEvent("page_finished_loading", {}, handleSendEventResponse, handleSendEventError);
+}
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    scrapeHeaders,
+    { urls: [PATTERN] },
+    ["requestHeaders", "extraHeaders"],
+);
+
+chrome.webNavigation.onCompleted.addListener(
+    handlePageFinishedLoading, 
+    {url: [
+        {urlMatches : PATTERN}
+    ]}
+);
